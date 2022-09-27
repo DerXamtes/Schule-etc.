daf17451d63e9e88f957062bda32562c0777f3a9
Derxamtes <ansorg.maximilian@gmail.com>
2022-09-27 12:34:10 +0200

    init

--- 2022.09/Test.java
+++ 2022.09/Test.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Test {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println();
+        Scanner sc = new Scanner(System.in);    
+        
+        System.out.println("Hello");
 
         sc.close();
         
