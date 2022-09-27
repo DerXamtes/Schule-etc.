7c03fa0620284c4bcc59e676eb05bd9bbd93d921
Derxamtes <ansorg.maximilian@gmail.com>
2022-09-27 09:58:51 +0200

    init

---
+++ 2022.09/Treibstoffverbrauch.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Treibstoffverbrauch {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Bitte geben Sie die Anzahl der LKWs ein");
+
+        double a = sc.nextDouble();
+
+        System.out.println("Bitte geben sie die Anzahl der Tage ein");
+
+        double b = sc.nextDouble();
+
+        System.out.println("Der Verbrauch liegt bei: " + a*b*4.5*35 + " Liter");
+
+        sc.close();        
+
+    }
+}
