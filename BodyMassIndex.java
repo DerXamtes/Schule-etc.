870c0db82748c72293570ff5c9305231792906d4
Derxamtes <ansorg.maximilian@gmail.com>
2022-09-27 09:37:27 +0200

    init

---
+++ BodyMassIndex.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class BodyMassIndex {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Bitte geben Sie Ihr Gewicht in Kilo ein");
+
+        double a = sc.nextDouble();
+
+        System.out.println("Bitte geben Sie Ihre Größe in Meter ein");
+
+        double b = sc.nextDouble();
+
+        System.out.print("Ihr BMI liegt bei: " + a/(b*b));
+
+        sc.close();
+        
+    }
+
+}
