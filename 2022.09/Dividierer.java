7d3d31ed987be2da2b247c27b0983f0bf450df69
DerXamtes <ansorg.maximilian@gmail.com>
2022-09-29 13:13:11 +0200

    cllean

--- 2022.09/Dividierer.java
+++ 2022.09/Dividierer.java
@@ -4,7 +4,6 @@ import java.math.RoundingMode;
 import java.text.DecimalFormat;
 
 public class Dividierer {
-  
   public static void main(String[] args) {
 
       Scanner sc = new Scanner(System.in);
