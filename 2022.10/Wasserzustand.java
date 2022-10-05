4b68aab53246679c5ab348d8e4c88287e73db024
Derxamtes <ansorg.maximilian@gmail.com>
2022-10-05 08:04:32 +0200

    idk

--- 2022.10/Wasserzustand.java
+++ 2022.10/Wasserzustand.java
@@ -1,4 +1,11 @@
+import java.util.Scanner;
+
 public class Wasserzustand {
-    
-    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        sc.close();
+        
+    }
 }
