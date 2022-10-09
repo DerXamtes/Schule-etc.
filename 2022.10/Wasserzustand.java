c6e068ebc9fe664de3d2abc57655df1502e7cb39
DerXamtes <114343566+DerXamtes@users.noreply.github.com>
2022-10-09 16:37:14 +0200

    init

--- 2022.10/Wasserzustand.java
+++ 2022.10/Wasserzustand.java
@@ -5,9 +5,37 @@ public class Wasserzustand {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Hello");
+        System.out.println("Welche Temperatur hat das Wasser?");
+
+        double Temperatur = sc.nextDouble();
+
+        if (Temperatur < 0) {
+
+            System.out.print("Das Wasser ist gefroren.");
+            } 
+
+        if (Temperatur == 0){
+
+            System.out.print("Das Wasser könnte sowohl gefroren als auch flüssig sein.");
+        }
+
+        if (Temperatur > 0 && Temperatur < 100) {
+
+            System.out.print("Das Wasser ist flüssig.");
+            } 
+
+        if (Temperatur == 100){
+
+            System.out.print("Das Wasser könnte sowohl flüssig als auch gasförmig sein.");
+        }
+
+        if (Temperatur > 100){
+
+            System.out.print("Das Wasser ist gasförmig.");
+        }
 
         sc.close();
         
     }
+
 }
