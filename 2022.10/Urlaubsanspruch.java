72f095832ed7d256ce690c9c3760b26803acf3e4
DerXamtes <114343566+DerXamtes@users.noreply.github.com>
2022-10-11 10:25:11 +0200

    init

---
+++ 2022.10/Urlaubsanspruch.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Urlaubsanspruch {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Bitte geben Sie Ihr Alter ein.");
+        int age = sc.nextInt();
+
+        System.out.println("Bitte geben Sie Ihr Behindertengrad in Prozent ein.");
+        int prozent = sc.nextInt();
+
+        System.out.println("Bitte geben Sie Ihre Betriebszugehörigkeit ein");
+        int zu = sc.nextInt();
+
+        int urlaubstage = 26;
+
+        if (age < 18 || age > 60) {
+
+            urlaubstage = 30;
+        }
+        
+        if (prozent >= 50) {
+
+            urlaubstage = urlaubstage + 5;
+        }
+        
+        if (zu >= 10) {
+
+            urlaubstage = urlaubstage + 2;
+        }
+
+        System.out.println(urlaubstage);
+
+        sc.close();
+    }
+}
