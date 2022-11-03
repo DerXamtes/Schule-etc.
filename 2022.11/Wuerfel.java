0b3a29caf0fcaf1492459cbf234d6f259206135b
DerXamtes <114343566+DerXamtes@users.noreply.github.com>
2022-11-03 11:16:02 +0100

    init

---
+++ 2022.11/Wuerfel.java
@@ -0,0 +1,16 @@
+public class Wuerfel{
+    public static void main(String[] args) {
+        
+        int a;
+        int b = 0;
+        
+        do {
+            a = ((int)Math.random()*6+1);
+            b = b + 1;
+            
+        } while (a != 6);
+
+        System.out.println(b);        
+    }
+
+}
