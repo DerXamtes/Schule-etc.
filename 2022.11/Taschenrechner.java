c18c5c8ca19921592a22fe1e53559ba469fd5ef8
DerXamtes <ansorg.maximilian@gmail.com>
2022-11-03 21:50:10 +0100

    idk

--- 2022.11/Taschenrechner.java
+++ 2022.11/Taschenrechner.java
@@ -9,57 +9,63 @@ public class Taschenrechner {
 
         System.out.println("Bitte geben Sie die erste zahl ein.");
 
-        while (!sc.hasNextDouble()) {
+        do {
+
+            while (!sc.hasNextDouble()) {
         
-            System.out.println("Bitte geben Sie eine zahl ein. ");
-            sc.nextLine();    
-        }
-          
+                System.out.println("Bitte geben Sie eine zahl ein. ");
+                sc.nextLine();    
+            }
+
             Double a = sc.nextDouble();
+            
+            System.out.println("Bitte geben Sie die zweite zahl ein.");        
 
-        System.out.println("Bitte geben Sie die zweite zahl ein.");        
-        
-        while (!sc.hasNextDouble()) {
-        
-            System.out.println("Bitte geben Sie eine zahl ein. ");
-            sc.nextLine();   
-        }
-          
-            Double b = sc.nextDouble();
+            while (!sc.hasNextDouble()) {
+            
+                System.out.println("Bitte geben Sie eine zahl ein. ");
+                sc.nextLine();   
+            }
+            
+                Double b = sc.nextDouble();
 
-        System.out.println("Bitte geben Sie den Operator ein(+,-,*,/).");
+            System.out.println("Bitte geben Sie den Operator ein ( +,-,*,/ ).");
 
-        String s = sc.next();
-        char operator = s.charAt(0);
+            String s = sc.next();
+            char operator = s.charAt(0);
+
+            switch (operator) {
+
+                case '+' : ergebnis = a+b; break;
+                case '-' : ergebnis = a-b; break;
+                case '*' : ergebnis = a*b; break;
+                case '/' : ergebnis = a/b; break;
+
+                default: System.out.println("Falscher Operator");
+            }
+
+            try {
+
+            if(ergebnis == Double.POSITIVE_INFINITY) {
+
+                throw new ArithmeticException();
+
+            } else {
+
+                System.out.println("Das Ergebnis ist: " + ergebnis);
+            }
+            
+            } catch (ArithmeticException e){
+
+                System.out.println("To Infinity and beyond!");
+            }
+
+            System.out.println("Um eine zweite Rechnug rechnen zu lassen, geben Sie die erste Zahl der nächsten rechnung ein.");              
+            System.out.println("Um den Taschenrechenr zu schließen geben sie Stopp ein.");
+
+        } while (sc.hasNextDouble());
 
         sc.close();
-
-        switch (operator) {
-
-            case '+' : ergebnis = a+b; break;
-            case '-' : ergebnis = a-b; break;
-            case '*' : ergebnis = a*b; break;
-            case '/' : ergebnis = a/b; break;
-
-            default: System.out.println("Falscher Operator");
-        }
-
-        try {
-
-        if(ergebnis == Double.POSITIVE_INFINITY) {
-
-            throw new ArithmeticException();
-
-        } else {
-
-            System.out.println("Das Ergebnis ist: " + ergebnis);
-        }
-        
-        } catch (ArithmeticException e){
-
-            System.out.println("To Infinity and beyond!");
-        }
-        
     }
 
 }
