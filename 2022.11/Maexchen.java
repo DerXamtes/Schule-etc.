93b8397acb393c4cf539680de53b0cc5d68e0b21
DerXamtes <114343566+DerXamtes@users.noreply.github.com>
2022-11-03 12:57:10 +0100

    init

---
+++ 2022.11/Maexchen.java
@@ -0,0 +1,16 @@
+public class Maexchen {
+    public static void main(String[] args) {
+
+        int punkte = 0;
+        int anzahlw = 0;
+        int anzahlm = 0;
+
+        while (punkte > 100000);
+
+            int a = (int)(Math.random()*6 +1);
+            int b = (int)(Math.random()*6 +1);
+            anzahlw = anzahlw + 1;
+            
+    }
+
+}
