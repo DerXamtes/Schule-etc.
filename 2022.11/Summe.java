dc47d9cc6e9f019558dd100e203cb1785c5011af
DerXamtes <114343566+DerXamtes@users.noreply.github.com>
2022-11-03 10:15:51 +0100

    init

---
+++ 2022.11/Summe.java
@@ -0,0 +1,15 @@
+public class Summe {
+    public static void main(String[] args) {
+
+        int zahl =0;
+        int summe =0;
+
+        while (summe <= 100) {
+
+            zahl = zahl+1;
+            summe = summe+zahl;
+        }
+
+        System.out.println(zahl);
+    }
+}
