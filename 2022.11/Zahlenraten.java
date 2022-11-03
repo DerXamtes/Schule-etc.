d140d23193f1f4ccbf3810ab062bef968fc21172
DerXamtes <114343566+DerXamtes@users.noreply.github.com>
2022-11-03 11:53:28 +0100

    init

---
+++ 2022.11/Zahlenraten.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Zahlenraten{
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int zufallszahl = (int)(Math.random()*100 + 1);
+        int ratezahl = 0;
+        int counter = 0;
+
+        while (zufallszahl != ratezahl){
+            
+            System.out.println("Bitte geben Sie Ihre Zahle ein (1-100)");
+            ratezahl = sc.nextInt();
+            counter = counter + 1;
+
+            if (ratezahl != zufallszahl) {
+
+                if (zufallszahl < ratezahl){
+
+                    System.out.println("Die Zufallszahl ist kleiner.");
+                
+                } else {
+
+                    System.out.println("Die Zufallszahl ist grösser.");
+                }
+            
+            } else {
+
+                System.out.print("Zufallszahl: " + zufallszahl + " Rateversuche: " +counter);
+            }
+            
+        }
+
+        sc.close();
+    }
+
+}
