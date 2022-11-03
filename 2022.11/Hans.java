4336eeea37e20b159ee8a6b86bf492f27676ca14
DerXamtes <114343566+DerXamtes@users.noreply.github.com>
2022-11-03 10:52:11 +0100

    init

---
+++ 2022.11/Hans.java
@@ -0,0 +1,14 @@
+public class Hans {
+    public static void main(String[] args) {
+        int a = 0;
+
+        do {
+
+            System.out.println("Ich muss immer meine Hausaufgaben machen.");
+            a = a +1;
+            
+        } while (a < 50);
+    
+    }
+
+}
