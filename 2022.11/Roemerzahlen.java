96e371aba31748b95b62d4b32a5c579dec142147
DerXamtes <114343566+DerXamtes@users.noreply.github.com>
2022-11-04 10:27:19 +0100

    idk

--- 2022.11/Roemerzahlen.java
+++ 2022.11/Roemerzahlen.java
@@ -6,6 +6,15 @@ public class Roemerzahlen {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Bitte geben Sie die Römische Zahl ein.");
+    
+        String a = sc.next();
+        String b = a.toLowerCase();
+
+        while (b != "stopp"){
+
+            
+
+        }
 
         sc.close();
     }
