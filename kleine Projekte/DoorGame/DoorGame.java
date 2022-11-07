89dac497f97ea53ec3c67b2b9ffd5f43a1c64ce0
DerXamtes <ansorg.maximilian@gmail.com>
2022-11-07 16:30:33 +0100

    idk

--- kleine Projekte/DoorGame/DoorGame.java
+++ kleine Projekte/DoorGame/DoorGame.java
@@ -1,5 +1,6 @@
 package DoorGame;
 
+import java.io.*;
 import java.util.Scanner;
 
 public class DoorGame {
@@ -7,10 +8,9 @@ public class DoorGame {
 
         Scanner sc = new Scanner(System.in);
 
+                    File highscore;
+
         String again;
-        int menu;
-        int door;
-        int random;
         int score = 0;
 
         System.out.println("Welcome to the Door Game! \n[1]Start \n[2]Highscore \n[3]Exit");
@@ -21,7 +21,7 @@ public class DoorGame {
             sc.nextLine();
         }
 
-        menu = sc.nextInt();
+        int menu = sc.nextInt();
 
         if (menu < 1 || menu > 3) {
 
@@ -46,7 +46,7 @@ public class DoorGame {
                 sc.nextLine();    
             }
 
-            door = sc.nextInt();
+            int door = sc.nextInt();
 
             while (door > 3 || door < 1) {
 
@@ -54,7 +54,7 @@ public class DoorGame {
                 door = sc.nextInt();
             }
 
-            random =  (int)(Math.random()*3 + 1);
+            int random =  (int)(Math.random()*3 + 1);
 
             if (door != random) {
 
@@ -64,15 +64,44 @@ public class DoorGame {
             
             } else {
 
-                System.out.println("Game Over! \nYour score is: " + score + "\nDo you want to play again? (y/n)");
-                
+                System.out.println("Game Over! \nYour score is: " + score);
+
+                            System.out.println("Do you want to save your score? (y/n)");
+
+                            String save = sc.next();
+                            save = save.toLowerCase();
+
+                            switch (save) {
+
+                                case "y" : case "yes" :
+
+                                    System.out.println("What is your name?");
+                                    String name = sc.next();
+
+                                    highscore = new File("Highscore.txt");
+
+                                    try {
+                                
+                                        BufferedWriter writer = new BufferedWriter(new FileWriter(highscore));
+                                        writer.write(score + " by " + name);
+                                        writer.close();
+                                
+                                    } catch(IOException ioe) {
+                                
+                                    System.err.println(ioe);
+                                }
+                            
+                            }
+
+                System.out.println("Do you want to play again? (y/n)");
                 again = sc.next(); 
                 again = again.toLowerCase();
 
+                score = 0;
+
                 switch (again) {
 
-                    case "y" : menu = 1; System.out.println("To choose a door type 1,2 or 3.\n[1][2][3]"); break;
-                    case "yes" : menu = 1; System.out.println("To choose a door type 1,2 or 3.\n[1][2][3]"); break;
+                    case "y" : case "yes" : menu = 1; System.out.println("To choose a door type 1,2 or 3.\n[1][2][3]"); break;
                     default : System.out.println("Thank you for playing! :)"); System.exit(0); break;
                 }
 
