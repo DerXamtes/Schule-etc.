55e76a72326f431a72fa30bd98e92a541e92a451
DerXamtes <114343566+DerXamtes@users.noreply.github.com>
2022-11-07 07:58:34 +0100

    idk

--- kleine Projekte/game/DoorGame.java
+++ kleine Projekte/game/DoorGame.java
@@ -19,6 +19,11 @@ public class DoorGame {
 
             door = sc.nextInt();
 
+            if (door < 4) {
+
+                System.out.println("Please enter a number between 1 and 3.");
+            }
+
             random =  (int)(Math.random()*3 + 1);
 
             if (door != random) {
