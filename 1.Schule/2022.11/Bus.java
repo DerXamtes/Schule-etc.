3a54ce33d2903e724c4822c5c7749cce11f88c36
DerXamtes <114343566+DerXamtes@users.noreply.github.com>
2022-11-10 11:30:19 +0100

    idk

---
+++ 1.Schule/2022.11/Bus.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Bus {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int door, light, moving;
+
+        System.out.println("Ist eine Tür offen? \n[1]Ja \n[2]Nein");
+        door = sc.nextInt();
+
+        if (door == 1) {
+
+            light = 1;
+
+            System.out.println("Bewegt sich der Bus? \n[1]Ja \n[2]Nein");
+
+            moving = sc.nextInt();
+
+            while (door == 1 && moving == 1) {
+
+                System.out.println("ALARRRM!!!");
+            }
+
+        } 
+
+        sc.close();
+    }
+
+}
