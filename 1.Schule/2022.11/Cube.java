7c4b96b80bc95d555bb8036c078e7c7f724daf89
DerXamtes <ansorg.maximilian@gmail.com>
2022-11-29 01:49:20 +0100

    idk

---
+++ 1.Schule/2022.11/Cube.java
@@ -0,0 +1,16 @@
+public class Cube {
+    public static void main(String[] args) {
+
+        for (int a = 0; a < 6; a++) {
+
+            for (int b = 0; b < 10; b++) {
+
+                System.out.print("x");
+            }
+
+            System.out.println();
+        }
+
+    }
+
+}
--- 1.Schule/2022.11/Test.java
+++ 1.Schule/2022.11/Test.java
@@ -1,16 +1,7 @@
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
 
-        for (int a = 0; a < 6; a++) {
-
-            for (int b = 0; b < 10; b++) {
-
-                System.out.print("x");
-            }
-
-            System.out.println();
-        }
-
+        System.out.printf("Hello %10d",20);
     }
 
 }
