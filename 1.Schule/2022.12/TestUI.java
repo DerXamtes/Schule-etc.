e1988b5842c36d8781e366af4b9e81e9cc7e946b
DerXamtes <DerXamtes@gmail.com>
2022-12-13 11:14:06 +0100

    idk

---
+++ 1.Schule/2022.12/TestUI.java
@@ -0,0 +1,33 @@
+public class TestUI {
+    public static int roemischNachArab (String roem) {
+
+        int arab = 0;
+        
+        for (int i = 0; i < roem.length(); i++) {
+
+            char zeichen = roem.charAt(i);
+
+            switch (zeichen) {
+
+                case 'I': arab += 1; break;
+                case 'V': arab += 5; break;
+                case 'X': arab += 10; break;
+                case 'L': arab += 50; break;
+                case 'C': arab += 100; break;
+                case 'D': arab += 500; break;
+                case 'M': arab += 1000; break;
+                
+                default: return -1;
+            }
+    
+        }
+        
+        return arab;
+    } 
+    
+    public static void main (String[] args) {
+
+        System.out.println(roemischNachArab("XLVIIII"));
+    }
+
+}
